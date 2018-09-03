7c392faf2bb72a80ebf4b1789c420d847f295ec0
QinxinTian <aptsubl@gmail.com>
2018-09-02 19:31:28 -0700

    create basic actions, connect basics, use of provider, create store, fetch.

---
+++ frontend/src/component/CategoryList.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+ const CategoryList = ({ categories }) => {
+  console.log("categories:", categories)
+  return (
+    <ul>
+      { categories.map((category, idx) =>
+          <li key={idx}>{category.name}</li>
+      )}
+    </ul>
+  );
+}
+ export default CategoryList;
