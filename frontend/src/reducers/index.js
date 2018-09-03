f830adfe4665840cf4ee727cfbc0dd63bf20eb3f
QinxinTian <aptsubl@gmail.com>
2018-09-03 16:41:41 -0700

    have separate reducers.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,45 +1,9 @@
 import { combineReducers } from 'redux'
- import {
-  RECEIVE_CATEGORIES
-} from '../actions'
- // here we define the intial state for the app
-const initialState = {
-  posts: [],
-  comments: [],
-  categories: [
-    {name: 'blog', path: 'blog'},
-    {name: 'video', path: 'video'},
-    {name: 'drops', path: 'drops'}]
-}
- const categoryReducer = (state = initialState, action) => {
-  console.log("state:", state)
-  console.log("action:", action)
-  switch (action.type) {
-    case RECEIVE_CATEGORIES:
-      const { categories } = action
-      return [...categories]
-    default:
-      return []
-  }
-}
- const PostReducer = (state = initialState, action) => {
-  console.log("state:", state)
-  console.log("action:", action)
-   switch (action.type) {
-    default:
-      return []
-  }
-}
- const ComentReducer = (state = initialState, action) => {
-  console.log("state:", state)
-  console.log("action:", action)
-   switch (action.type) {
-    default:
-      return []
-  }
-}
+import CategoryReducer from './categoryReducer'
+import PostReducer from './postReducer'
+import CommentReducer from './commentReducer'
  export default combineReducers({
-  categories: categoryReducer,
+  categories: CategoryReducer,
   posts: PostReducer,
-  comments: ComentReducer
+  comments: CommentReducer
 })
